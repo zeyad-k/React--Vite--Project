9b3f754c72d6fcf85cf0783de3f3de0bc08aba74
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-06-24 22:12:05 +0300

    Refactor TodoAppWithAtom and TodoAppWithRedux components to use consistent styling

--- src/components/AtomAppComponents/TodoAppWithAtom.tsx
+++ src/components/AtomAppComponents/TodoAppWithAtom.tsx
@@ -3,8 +3,8 @@ import TodosListAtom from "./TodosListAtom";
 
 const TodoAppWithAtom = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-4 bg-white rounded-lg shadow-md">
+    <div className="flex flex-col items-center justify-center  ">
+      <div className="w-full max-w-md p-4 bg-gray-100 rounded-lg shadow-md">
         <InputFormRecoil />
         <TodosListAtom />
       </div>
--- src/components/ReduxAppComponents/TodoAppWithRedux.tsx
+++ src/components/ReduxAppComponents/TodoAppWithRedux.tsx
@@ -8,8 +8,8 @@ const TodoAppWithRedux = () => {
   
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-4 bg-white rounded-lg shadow-md">
+    <div className="flex flex-col items-center justify-center  ">
+      <div className="w-full max-w-md p-4 bg-gray-100 rounded-lg shadow-md">
         <InputFormRedux />
         <TodosListRedux />
       </div>
