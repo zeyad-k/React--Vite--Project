bbd90a25f17a1ffe782f2110b12bd42024c1faa4
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-06-24 15:24:44 +0300

    Refactor todoSlice reducer and remove commented code

--- src/ReduxToolkit/todoSlice.ts
+++ src/ReduxToolkit/todoSlice.ts
@@ -1,36 +1,3 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const todoSlice = createSlice({
-//   name: "todos",
-//   initialState: [],
-//   reducers: {
-//     addTodo: (state, action) => {
-//       const newTodo = {
-//         id: Date.now(),
-//         text: action.payload,
-//         completed: false,
-//       };
-//       state.push(newTodo);
-//     },
-//     toggleComplete: (state, action) => {
-//       const todo = state.find((todo) => todo.id === action.payload);
-//       if (todo) {
-//         todo.completed = !todo.completed;
-//       }
-//     },
-//     deleteTodo: (state, action) => {
-//       const index = state.findIndex((todo) => todo.id === action.payload);
-//       if (index !== -1) {
-//         state.splice(index, 1);
-//       }
-//     },
-//   },
-// });
-// export const { addTodo, toggleComplete, deleteTodo } = todoSlice.actions;
-// export default todoSlice.reducer;
-
-
-
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 // Define a type for the todo items
