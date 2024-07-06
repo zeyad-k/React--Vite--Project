ceafac330602b31e276f857d9d0ea9fac77ed383
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-07-07 00:17:42 +0300

    Refactor TodoInputForm component to use lowercase for the "add" button label

--- src/components/TodoInputForm.tsx
+++ src/components/TodoInputForm.tsx
@@ -32,7 +32,7 @@ const TodoInputForm: React.FC<TodoInputFormProps> = ({ addTodo }) => {
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         type="submit"
       >
-        {t("Add")}
+        {t("add")}
       </button>
     </form>
   );
